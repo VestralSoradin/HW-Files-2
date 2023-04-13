cb48aaa141fb676c9840f69d60ed1e36f7c0378a
Константин Пигулевский <pigulevskii2000@gmail.com>
2023-04-13 23:47:42 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+with open('1.txt') as f:
+     result = f.readlines()
+     print(len(result))
+
+with open('2.txt') as f:
+    result2 = f.readlines()
+    print(len(result2))
+
+with open('3.txt') as f:
+    result3 = f.readlines()
+    print(len(result3))
